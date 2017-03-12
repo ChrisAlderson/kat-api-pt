860d2c1b96694c54ee0734ed146b3edcec67254f
Chris Alderson <chrisalderson@protonmail.com>
2017-03-12 21:07:42 +0100

    updated tests for katcr.co

--- test/kat.js
+++ test/kat.js
@@ -1,29 +1,27 @@
-const chai = require('chai');
-const assert = chai.assert;
-const KAT = require("../kat-api-pt");
+'use strict';
 
-describe("KAT", () => {
+const { assert } = require('chai');
+const KAT = require('../kat-api-pt');
+
+describe('KAT', () => {
 
   let kat;
   before(() => kat = new KAT());
 
-  it("Should get results with a simple search", done => {
-    kat.search("Game of Thrones").then(res => {
+  it('Should get results with a simple search', done => {
+    kat.search('Game of Thrones').then(res => {
       assert.isObject(res);
       done();
     }).catch(err => done(err));
   });
 
-  it("Should get results with an advanced search", done => {
+  it('Should get results with an advanced search', done => {
     kat.search({
-      query: "Game of Thrones",
-      category: "tv",
-      min_seeds: "3",
-      uploader: "ettv",
-      sort_by: "seeders",
-      order: "desc",
-      verified: 1,
-      language: "en",
+      query: 'Game of Thrones',
+      category: 'tv',
+      sort_by: 'seeders',
+      order: 'desc',
+      language: 'english',
       page: 1
     }).then(res => {
       assert.isObject(res);
@@ -31,16 +29,13 @@ describe("KAT", () => {
     }).catch(err => done(err));
   });
 
-  it("Should not get results with an advanced search", done => {
+  it('Should not get results with an advanced search', done => {
     kat.search({
-      query: "Game of Thrones",
-      category: "tv",
-      min_seeds: "666",
-      uploader: "ettv",
-      sort_by: "seeders",
-      order: "desc",
-      verified: 1,
-      language: "en"
+      query: 'Game of Thrones',
+      category: 'tv',
+      sort_by: 'seeders',
+      order: 'desc',
+      language: 'english'
     }).then(res => {
       assert.isObject(res);
       done();
