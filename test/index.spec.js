687d8ac36f35e7199ce4bd8ca6a37b020545ebab
Chris Alderson <chrisalderson@protonmail.com>
2017-10-15 12:44:13 +0000

    add test for no results

--- test/index.spec.js
+++ test/index.spec.js
@@ -44,6 +44,20 @@ describe('KatApi', () => {
   })
 
   /** @test {KatApi#search} */
+  it('Should get results with an advanced search', done => {
+    kat.search({
+      query: '"this probably won\'t return any results"',
+      category: 'tv_other',
+      sortBy: 'seeders',
+      orderBy: 'desc',
+      language: 'english',
+      page: 1
+    }).then(res => {
+      expect(res).to.be.an('object')
+      done()
+    }).catch(err => done(err))
+  })
+  /** @test {KatApi#search} */
   it('should reject the promise to search with an undefined as a parameter', done => {
     kat.search(undefined).then(done)
       .catch(err => {
